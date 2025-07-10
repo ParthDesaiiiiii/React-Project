fd57d4f1921bcafcf5ae8d92617e992733037621
ParthDesaiiiiii <118049894+ParthDesaiiiiii@users.noreply.github.com>
2025-07-10 12:53:04 -0400

    Deployment using git pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //Whether Dark mode is enabled or not
@@ -40,17 +40,19 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
       <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert}/>
 
       <div className="container my-3">
-      <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
-          </Routes>
+      {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />} /> */}
+            {/* <Route exact path="/" element={ */}
+            <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+            {/* } />
+          </Routes> */}
       </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
